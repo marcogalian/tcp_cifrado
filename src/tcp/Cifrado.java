a0afcce19ed82ee30d1470a5255f0fc808d7ae98
marco <marcogalian@hotmail.com>
2026-01-21 10:31:30 +0100

    Agregados comentarios a la clase servidor para entender todo un poco mejor

--- src/tcp/Cifrado.java
+++ src/tcp/Cifrado.java
@@ -12,19 +12,36 @@ public class Cifrado {
     // Para AES, debe tener exactamente 16, 24 o 32 caracteres (bytes)
     private static final String CLAVE = "ABC_123_SECRET_K";
 
+
+    // MÉTODO PARA ENCRIPTAR (Cerrar la caja fuerte)
     public static String encriptar(String datos) throws Exception {
+        // A. Preparamos la llave: Convertimos nuestro texto en una "llave real" que Java entienda
         SecretKeySpec sk = new SecretKeySpec(CLAVE.getBytes(), ALGORITMO);
+        // B. Pedimos la herramienta: Obtenemos una instancia del motor de cifrado AES
         Cipher c = Cipher.getInstance(ALGORITMO);
+        // C. Configuramos el modo: Le decimos al motor que su trabajo ahora es ENCRIPTAR (Cerrar)
         c.init(Cipher.ENCRYPT_MODE, sk);
+        // D. Ejecutamos la acción: El texto se convierte en un montón de bytes ilegibles
         byte[] enc = c.doFinal(datos.getBytes());
+
+        // E. Envoltorio de seguridad (Base64): Los bytes raros se pasan a texto normal (A-Z, 0-9)
+        // para que viajen por el cable sin romperse
         return Base64.getEncoder().encodeToString(enc);
     }
 
+    // MÉTODO PARA DESENCRIPTAR (abrir la caja fuerte)
     public static String desencriptar(String datosCifrados) throws Exception {
+        // A. Igual que antes, preparamos la llave (debe ser la misma)
         SecretKeySpec sk = new SecretKeySpec(CLAVE.getBytes(), ALGORITMO);
+        // B. Pedimos la misma herramienta (AES)
         Cipher c = Cipher.getInstance(ALGORITMO);
+        // C. Configuramos el modo: Ahora le decimos que su trabajo es DESENCRIPTAR (Abrir)
         c.init(Cipher.DECRYPT_MODE, sk);
+        // D. Se le quita el envoltorio: Pasamos de texto Base64 a los bytes encriptados originales
         byte[] dec = Base64.getDecoder().decode(datosCifrados);
+
+        // E. Abrimos la caja: El motor usa la llave para recuperar el texto original en bytes
+        // y lo transformamos de nuevo en un String legible
         return new String(c.doFinal(dec));
     }
 }
