6510aaedf8133b0026a14e789414df87d6643ecc
JuanJose-96 <jjsaVIXXXIII@gmail.com>
2026-01-21 20:05:17 +0100

    Creacion clase config

---
+++ src/tcp/Config.java
@@ -0,0 +1,8 @@
+package tcp;
+
+public class Config {
+    // Configuración del servidor
+    public static final String SERVER_HOSTNAME = "Marco.local";
+    public static final int SERVER_PORT = 8080;
+
+}
